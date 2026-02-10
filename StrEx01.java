bd1bfff467c5b0f6db3a72da5926322ec75d0991
Elizaped <ssrithon.s@ku.th>
2026-02-10 21:23:56 +0700

    Add StrEx01.java to demonstrate string comparison

---
+++ StrEx01.java
@@ -0,0 +1,9 @@
+public class StrEx01 {
+    public static void main(String[] args) {
+        String a = "java";
+        String b = "java";
+        String c = new String("java"); 
+        System.out.println(a==b);
+        System.out.println(a==c);   
+    }
+}
