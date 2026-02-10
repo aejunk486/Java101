1aa46fa13ba040418b2740965862daa31ca67abb
Elizaped <ssrithon.s@ku.th>
2026-02-10 21:46:52 +0700

    Update StrEx01.java

--- StrEx01.java
+++ StrEx01.java
@@ -1,9 +1,10 @@
 public class StrEx01 {
     public static void main(String[] args) {
-        String a = "java";
-        String b = "java";
-        String c = new String("java"); 
-        System.out.println(a==b);
-        System.out.println(a==c);   
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new String("Hi"); 
+        String s4 = "Hi";
+        System.out.println("s1==s2: "+(s1==s2));
+        System.out.println("s3==s4: "+(s3==s4));   
     }
 }
