7128b7c3ea9525d00e2b2853f8e68b758f4692b2
Elizaped <ssrithon.s@ku.th>
2026-03-01 16:21:15 +0700

    Add files via upload

---
+++ DinamicArray2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class DinamicArray2 {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add("3");
+        System.out.println(arrayList);
+      
+    }
+}
+
