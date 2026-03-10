523743db4fddb37e343924fa6f9cc5dbf7858ae5
Elizaped <ssrithon.s@ku.th>
2026-03-10 16:10:17 +0700

    Add files via upload

---
+++ ClassLab01.java
@@ -0,0 +1,16 @@
+  import java.util.*;
+ class ClassLab01{
+      public static void main(String[] args) {
+          show(); 
+     }
+      static void show(){
+         Scanner sc = new Scanner(System.in);
+          System.out.print("Insert DayName: ");
+          String day = sc.nextLine();
+        System.out.println("Today: " +day);
+     }
+ }
+
+
+
+ 
